9ae2683aedc0bbf1dc6d1e9337576440c6774239
Abdulrahman-Bayoumi <83184593+Abdulrahman-Bayoumi@users.noreply.github.com>
2022-11-06 16:18:37 +0200

    OMR

---
+++ OMRChecker-master/main.py
@@ -0,0 +1,83 @@
+"""
+
+ OMRChecker
+
+ Author: Udayraj Deshmukh
+ Github: https://github.com/Udayraj123
+
+"""
+
+from src.logger import logger
+
+logger.info(f"Loading OMRChecker modules...")
+# It takes a few seconds for the imports
+
+import argparse
+from pathlib import Path
+
+from src.core import entry_point
+
+# construct the argument parse and parse the arguments
+argparser = argparse.ArgumentParser()
+
+argparser.add_argument(
+    "-i",
+    "--inputDir",
+    default=["inputs"],
+    # https://docs.python.org/3/library/argparse.html#nargs
+    nargs="*",
+    required=False,
+    type=str,
+    dest="input_paths",
+    help="Specify an input directory.",
+)
+
+argparser.add_argument(
+    "-o",
+    "--outputDir",
+    default="outputs",
+    required=False,
+    dest="output_dir",
+    help="Specify an output directory.",
+)
+
+# TODO: separate the interactive modes from main code
+argparser.add_argument(
+    "-a",
+    "--autoAlign",
+    required=False,
+    dest="autoAlign",
+    action="store_true",
+    help="(experimental) Enables automatic template alignment - \
+    use if the scans show slight misalignments.",
+)
+
+argparser.add_argument(
+    "-l",
+    "--setLayout",
+    required=False,
+    dest="setLayout",
+    action="store_true",
+    help="Set up OMR template layout - modify your json file and \
+    run again until the template is set.",
+)
+
+
+(
+    args,
+    unknown,
+) = argparser.parse_known_args()
+args = vars(args)
+
+# FIX: remove join
+if len(unknown) > 0:
+    logger.warning("".join(["\nError: Unknown arguments: ", unknown]))
+    argparser.print_help()
+    exit(11)
+
+for root in args["input_paths"]:
+    entry_point(
+        Path(root),
+        Path(root),
+        args,
+    )
